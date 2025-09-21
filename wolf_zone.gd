416786a1c908c2f36f24209261ea5f032c553ead
ph4nt0m <marija.ilievskax@gmail.com>
2025-09-21 19:22:08 +0200

    Wolf warning zone

---
+++ wolf_zone.gd
@@ -0,0 +1,22 @@
+extends Area3D
+
+@onready var dialogue = preload("res://dialogues/wolves.dialogue")
+@onready var player = $"../../../Player"
+
+var warning = true
+
+func _on_body_entered(body):
+	if warning:
+		warning = false
+		_start_dialogue(dialogue, "start")
+
+func _start_dialogue(dialogue: DialogueResource, start_node: String):
+	player.can_move = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	DialogueManager.show_dialogue_balloon(dialogue, start_node)
+	await DialogueManager.dialogue_ended
+	_on_dialogue_ended()
+
+func _on_dialogue_ended():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	player.can_move = true
