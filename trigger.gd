17bbd7409703ca8c3063ba419ea5426b85b750f7
ph4nt0m <marija.ilievskax@gmail.com>
2025-09-21 19:22:52 +0200

    Radiation dialogue

--- trigger.gd
+++ trigger.gd
@@ -20,10 +20,12 @@ func _on_body_entered(body):
 
 func _start_dialogue(dialogue: DialogueResource, start_node: String):
 	player.can_move = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	DialogueManager.show_dialogue_balloon(dialogue, start_node)
 	await DialogueManager.dialogue_ended
 	_on_dialogue_ended()
 
 func _on_dialogue_ended():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	repaired = true
 	player.can_move = true
