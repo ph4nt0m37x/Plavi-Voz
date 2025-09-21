b63154628024f3cde26deb0dd8d28fc21941573a
ph4nt0m <marija.ilievskax@gmail.com>
2025-09-21 19:23:31 +0200

    End game script

---
+++ end_game.gd
@@ -0,0 +1,21 @@
+extends Area3D
+
+@onready var dialogue = preload("res://dialogues/endgame.dialogue")
+@onready var player = $"../../../Player"
+
+var warning = true
+
+func _on_body_entered(body):
+	if warning:
+		warning = false
+		_start_dialogue(dialogue, "start")
+
+func _start_dialogue(dialogue: DialogueResource, start_node: String):
+	player.can_move = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	DialogueManager.show_dialogue_balloon(dialogue, start_node)
+	await DialogueManager.dialogue_ended
+	_on_dialogue_ended()
+
+func _on_dialogue_ended():
+	get_tree().quit()
