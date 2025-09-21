025dbc49cff2d4975b850cb3b678e451a5fca3e5
ph4nt0m <marija.ilievskax@gmail.com>
2025-09-21 13:05:48 +0200

    Added trigger

---
+++ trigger.gd
@@ -0,0 +1,28 @@
+extends Area3D
+
+@onready var dialogue = preload("res://dialogues/radio.dialogue")
+
+@onready var player = $"../../Player"
+
+var repaired := false
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
+
+func _on_body_entered(player) -> void:
+	if not repaired:
+		_start_dialogue(dialogue, "start")
+		
+
+func _start_dialogue(dialogue: DialogueResource, start_node: String):
+	player.can_move = false
+	DialogueManager.show_dialogue_balloon(dialogue, start_node)
+	await DialogueManager.dialogue_ended
+	_on_dialogue_ended()
+
+func _on_dialogue_ended():
+	repaired = true
+	player.can_move = true
